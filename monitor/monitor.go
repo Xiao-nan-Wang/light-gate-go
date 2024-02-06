06aaf6e40fefbafdbe0a4ff5eec5c3ff5d9fc9af
lord <1023717264@qq.com>
2024-02-07 00:25:16 +0800

    正在增加请求统计

---
+++ monitor/monitor.go
@@ -0,0 +1,6 @@
+package monitor
+
+var accessPerSecond = make(map[int16]int64)
+var accessPerMinute = make(map[int16]int64)
+var accessPerHour = make(map[int16]int64)
+var accessPerDay = make(map[int16]int64)
